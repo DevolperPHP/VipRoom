58640747e5017a30ca23b39db70e51ff48491393
DevolperPHP <devolperphp@gmail.com>
2016-09-07 12:38:56 +0300

    Update Main.php

--- VipRoomDP/src/Main.php
+++ VipRoomDP/src/Main.php
@@ -3,39 +3,47 @@
 namespace VipRoomDP;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\level\Position;
-use pocketmine\permission;
+use pocketmine\permission\Permission;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\utils\Config;
 
-class ExpLevel extends PluginBase{
-  
-  public function onEnable(){
-    $this->getServer()->getLogger()->info(Color::GREEN."Plugin Is On");
-    @mkdir($this->getDataFolder());
-  }
-  
-  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-    switch($cmd->getName()){
-      case 'viproom':
-        $x = $sender->getFloorX();
-        $y = $sender->getFloorY();
-        $z = $sender->getFloorZ();
-        $this->getConfig()->get("x1", $x);
-        $this->getConfig()->get("y1", $y);
-        $this->getConfig()->get("z1", $z);
-        if($sender->hasPermission("viproomdp.viproom")){
-          $sender->teleport(new position($x, $y, $z));
-          $this->getConfig()->save();
-          $sender->sendMessage(Color::AQUA."Welcome To VIP Room");
-        } else {
-          $sender->sendMessage(Color::RED."You Not VIP");
-        }
-    }
-  }
-  
-  public function onDisable(){
-    $this->getConfig()->save();
+class Main extends PluginBase implements Listener{
+	
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		@mkdir($this->getDataFolder());
+		$xyz = [
+				"x" => 0,
+				"y" => 0,
+				"z" => 0,
+		];
+		$cfg = new Config($this->getDataFolder(), "Config.yml", Config::YAML, $xyz);
+		$cfg->save();
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+			case 'viproom':
+				if($sender->hasPermission("viproomdp.viproom")){
+					$x = $this->getConfig()->get("x");
+					$y = $this->getConfig()->get("y");
+					$z = $this->getConfig()->get("z");
+					$sender->teleport(new position($x, $y, $z));
+					$this->getConfig()->save();
+					$sender->sendMessage(Color::AQUA."[VipRoomDP] Welcome To Vip Room");
+				} else {
+					$sender->sendMessage(Color::RED."[VipRoomDP] You Not VIP");
+				}
+				
+		}
+	}
+	
+	public function onDisable(){
+		$this->getConfig()->save();
+	}
 }
